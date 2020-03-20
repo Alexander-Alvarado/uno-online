e47915ac6b2a04a1f68aba58ec0a5a9ed2a5bb48
Alex Alvarado <alexjalvarado66@gmail.com>
2020-03-20 08:33:00 -0400

    fixed 404 issue

--- public/uno.js
+++ public/uno.js
@@ -1,6 +1,4 @@
-var socket = io.connect(
-  "http://localhost:5000/" || "https://online-uno.herokuapp.com/"
-);
+var socket = io.connect("https://online-uno.herokuapp.com/");
 
 $(function() {
   $("main").hide();
